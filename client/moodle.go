54daaeb6e50a0ab8b6ef5ca048bdd87f215c21e6
Asaf Ohayon <asaf@sysbind.co.il>
2021-01-06 14:18:04 +0200

    query db concurrently, close connection between scrapes, k8s example

--- client/moodle.go
+++ client/moodle.go
@@ -1,16 +1,19 @@
 package client
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"strconv"
+	"sync"
 	"time"
 
 	"github.com/jackc/pgx"
+	"github.com/jackc/pgx/v4/pgxpool"
 )
 
 type Moodle struct {
-	Connection *pgx.Conn // defer Connection.close()
+	PoolConfig *pgxpool.Config
 	Shortname  string
 }
 
@@ -21,39 +24,85 @@ type UserStats struct {
 
 func New(hostname string, username string, password string, database string) (moodle *Moodle, err error) {
 	moodle = nil
-	conn, err := pgx.Connect(
-		pgx.ConnConfig{Host: hostname, User: username, Password: password, Database: database})
+	connconf := pgx.ConnConfig{Host: hostname, User: username, Password: password, Database: database}
+	conn, err := pgx.Connect(connconf)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		return
 	}
+	defer conn.Close()
 
 	var shortname string
 	err = conn.QueryRow("SELECT shortname FROM mdl_course WHERE id=1").Scan(&shortname)
-
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
 		return
 	}
-	moodle = &Moodle{Connection: conn, Shortname: shortname}
+
+	connstr := fmt.Sprintf("postgres://%s:%s@%s:5432/%s",
+		username, password, hostname, database)
+	poolconf, err := pgxpool.ParseConfig(connstr)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed configuring connection pool: %v\n", err)
+		return
+	}
+	moodle = &Moodle{PoolConfig: poolconf, Shortname: shortname}
 
 	return
+}
+
+func GetLiveUsers(ctx context.Context, conn *pgxpool.Pool,
+	stats *UserStats, wg *sync.WaitGroup) {
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		before5minutes := strconv.Itoa(int(time.Now().Unix() - 300))
+		var count int
+		if err := conn.QueryRow(ctx, "SELECT COUNT(id) FROM mdl_user WHERE lastaccess >"+before5minutes).Scan(&count); err != nil {
+			fmt.Fprintf(os.Stderr, "GetExpectedUpcomingExamParticipants failed: %v\n", err)
+			return
+		}
+		stats.LiveUsers = count
+	}()
+}
+
+func GetExpectedUpcomingExamParticipants(ctx context.Context,
+	conn *pgxpool.Pool, stats *UserStats, wg *sync.WaitGroup) {
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		more10minutes := strconv.Itoa(int(time.Now().Unix() + 600))
+		var count int
+
+		err := conn.QueryRow(ctx, "SELECT COUNT (u.id) FROM mdl_quiz q JOIN mdl_course c ON c.id = q.course JOIN mdl_enrol e ON e.courseid = c.id JOIN mdl_user_enrolments ue ON ue.enrolid = e.id JOIN mdl_user u ON u.id = ue.userid WHERE (q.timeclose - q.timeopen) < 60*60*10 AND q.timeopen <"+more10minutes+" AND q.timeclose >"+more10minutes).Scan(&count)
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "GetExpectedUpcomingExamParticipants failed: %v\n", err)
+			return
+		}
+		stats.ExpectedUpcomingExamParticipants = count
+	}()
 }
 
 func (m *Moodle) GetUserStats() (stats *UserStats, err error) {
 	stats = &UserStats{LiveUsers: 0, ExpectedUpcomingExamParticipants: 0}
 
-	before5minutes := strconv.Itoa(int(time.Now().Unix() - 300))
-	err = m.Connection.QueryRow("SELECT COUNT(id) FROM mdl_user WHERE lastaccess >" + before5minutes).Scan(&stats.LiveUsers)
+	ctx := context.Background()
+	conn, err := pgxpool.ConnectConfig(ctx, m.PoolConfig)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		return
 	}
-	more10minutes := strconv.Itoa(int(time.Now().Unix() + 600))
-	err = m.Connection.QueryRow("SELECT COUNT (u.id) FROM mdl_quiz q JOIN mdl_course c ON c.id = q.course JOIN mdl_enrol e ON e.courseid = c.id JOIN mdl_user_enrolments ue ON ue.enrolid = e.id JOIN mdl_user u ON u.id = ue.userid WHERE (q.timeclose - q.timeopen) < 60*60*10 AND q.timeopen <" + more10minutes + " AND q.timeclose >" + more10minutes).Scan(&stats.ExpectedUpcomingExamParticipants)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
-		return
-	}
+	defer conn.Close()
+
+	var wg sync.WaitGroup
+
+	GetLiveUsers(ctx, conn, stats, &wg)
+	GetExpectedUpcomingExamParticipants(ctx, conn, stats, &wg)
+
+	wg.Wait()
+
 	return
 }
