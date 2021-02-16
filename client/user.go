742b743dfc29a845df4041cd6c08ee67312ecc1d
Asaf Ohayon <asaf@sysbind.co.il>
2021-02-16 13:33:09 +0200

    expected upcoming: eliminate duplicate user records

--- client/user.go
+++ client/user.go
@@ -63,7 +63,7 @@ func getExpectedUpcomingExamParticipants(ctx context.Context,
 		more20minutes := strconv.Itoa(int(time.Now().Unix() + 1200))
 		var count int
 
-		err := conn.QueryRow(ctx, "SELECT COUNT (u.id) FROM mdl_quiz q JOIN mdl_course c ON c.id = q.course JOIN mdl_enrol e ON e.courseid = c.id JOIN mdl_user_enrolments ue ON ue.enrolid = e.id JOIN mdl_user u ON u.id = ue.userid WHERE u.lastaccess <"+before5minutes+" AND (q.timeclose - q.timeopen) < 60*60*5 AND q.timeopen <"+more20minutes+" AND q.timeclose >"+more20minutes).Scan(&count)
+		err := conn.QueryRow(ctx, "SELECT COUNT (DISTINCT(u.id)) FROM mdl_quiz q JOIN mdl_course c ON c.id = q.course JOIN mdl_enrol e ON e.courseid = c.id JOIN mdl_user_enrolments ue ON ue.enrolid = e.id JOIN mdl_user u ON u.id = ue.userid WHERE u.lastaccess <"+before5minutes+" AND (q.timeclose - q.timeopen) < 60*60*5 AND q.timeopen <"+more20minutes+" AND q.timeclose >"+more20minutes).Scan(&count)
 
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "GetExpectedUpcomingExamParticipants failed: %v\n", err)
