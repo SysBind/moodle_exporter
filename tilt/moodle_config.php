f4582e25aecd6b3baad50ee91d90aaf1b97d4c93
Asaf Ohayon <asaf@sysbind.co.il>
2025-02-13 15:44:54 +0200

    tilt: default configurtion for moodle

---
+++ tilt/moodle_config.php
@@ -0,0 +1,29 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'pgsql';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'postgresql';
+$CFG->dbname    = 'postgres';
+$CFG->dbuser    = 'postgres';
+$CFG->dbpass    = 'q1w2e3r4';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => '',
+  'dbsocket' => '',
+);
+
+$CFG->wwwroot   = 'http://localhost:8001';
+$CFG->dataroot  = '/var/www/moodledata';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
